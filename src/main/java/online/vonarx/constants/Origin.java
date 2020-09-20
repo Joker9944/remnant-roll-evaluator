e89b5c25c2578875605fc0078037382b27b126c8
Joker9944 <9194199+joker9944@users.noreply.github.com>
2020-09-20 16:58:48 +0200

    rename actor origins

--- src/main/java/online/vonarx/constants/Origin.java
+++ src/main/java/online/vonarx/constants/Origin.java
@@ -5,12 +5,12 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Origin {
-	MAIN_GAME("Main Game"),
-	ADVENTURE_MODE_UPDATE("AM Update"),
-	LETO_S_LAB_UPDATE("LL Update"),
-	HARDCORE_UPDATE("HC Update"),
-	SWAMPS_OF_CORSUS_UPDATE("SoC Update"),
-	SUBJECT_2923_UPDATE("Subject 2923 Update"),
+	FROM_THE_ASHES("From The Ashes"),
+	ADVENTURE_MODE_UPDATE("Adventure Mode Update"),
+	LETO_S_LAB_UPDATE("Leto's Lab Update"),
+	HARDCORE_UPDATE("Hardcore Update"),
+	SWAMPS_OF_CORSUS_DLC("Swamps of Corsus DLC"),
+	SUBJECT_2923_DLC("Subject 2923 DLC"),
 	UNKNOWN("Unknown");
 
 	@Getter private final String displayName;
